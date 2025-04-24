c937e0d1434b789ebc6f5483f429b8070ccdfabd
Frysperz <109074111+Frysperz@users.noreply.github.com>
2025-04-24 18:09:18 +0200

    Create players.dart

---
+++ lib/pages/players.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Players extends StatelessWidget {
+  const Players({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Liste des joueurs') ,
+      ),
+      body: Center(
+        child: ListView(
+						children: [
+							Card(
+								child: ListTile(
+									leading: Image.asset("assets/images/logof.png"),
+									title: Text("Cypcyp"), 
+									subtitle: Text("créateur"), 
+								), 
+							), 
+							Card(
+								child: ListTile(
+									leading: Image.asset("assets/images/logof.png"),
+									title: Text("Le Bouton d'Or"), 
+									subtitle: Text("Challengeuse"), 
+								), 
+							),  
+							Card(
+								child: ListTile(
+									leading: Image.asset("assets/images/logof.png"),
+									title: Text("La Grande & Le Jeannot Junior"), 
+									subtitle: Text("Parents"), 
+								), 
+							), 
+							Card(
+								child: ListTile(
+									leading: Image.asset("assets/images/logof.png"),
+									title: Text("La Sisou de Papanou"), 
+									subtitle: Text("Voyageuse"), 
+								), 
+							), 
+							Card(
+								child: ListTile(
+									leading: Image.asset("assets/images/logof.png"),
+									title: Text("Les Gauthier Juniors"), 
+									subtitle: Text("Quoicoubeh"), 
+								), 
+							), 
+							Card(
+								child: ListTile(
+									leading: Image.asset("assets/images/logof.png"),
+									title: Text("Oncle H"), 
+									subtitle: Text("VVette"), 
+								), 
+							), 
+							Card(
+								child: ListTile(
+									leading: Image.asset("assets/images/logof.png"),
+									title: Text("Le Géniteur"), 
+									subtitle: Text("Géniteur"), 
+								), 
+							), 
+						], 
+					), 
+      ),
+    );
+  }
+}
