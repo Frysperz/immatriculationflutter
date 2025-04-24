b8769a50b2be60ffda954ea15075e433eda4dfc7
Frysperz <109074111+Frysperz@users.noreply.github.com>
2025-04-24 21:00:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,54 @@ import 'package:immatriculationflutter/pages/home_page.dart';
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+} 
+
+class _MyAppState extends State<MyApp> {
+
+  int _currentIndex = 0;
+
+  setCurrentIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  } 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Dernière Plaque"), 
+        ),
+        body: [
+          HomePage(),
+          Players()
+        ][_currentIndex], 
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setCurrentIndex(index), 
+          selectedItemColor: Colors.blue, 
+          unselectedItemColor: Colors.grey,
+          iconSize: 32,
+          elevation: 10,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Accueil'
+            ), 
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month),
+              label: 'Joueurs'
+            ), 
+          ], 
+        ), 
+      ),
     );
   }
 }
