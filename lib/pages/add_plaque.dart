9df9466532c1b15796ef3c9dce7314e98624dd0e
Frysperz <109074111+Frysperz@users.noreply.github.com>
2025-04-25 07:56:56 +0200

    Update add_plaque.dart

--- lib/pages/add_plaque.dart
+++ lib/pages/add_plaque.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
-class AddPlaque extends StatelessWidget {
+class AddPlaque extends StatefulWidget {
   const AddPlaque({super.key});
 
   @override
+  State<AddPlaque> createState() => _AddPlaqueState();
+
+} 
+
+class _AddPlaqueState extends State<AddPlaque> {
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text('Ajouter une nouvelle plaque'), 
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Nouvelle Plaque', 
+                hintText: 'AA-123-AA', 
+                border: OutlineInputBorder(), 
+              ), 
+            ), 
+            SizedBox(
+              width: double.infinity, 
+              height: 50,
+              child: ElevatedButton(
+                onPressed:, 
+                child: Text("Envoyer") 
+              ), 
+            ) 
+          ], 
+        ), 
+      ), 
     );
   }
 }
